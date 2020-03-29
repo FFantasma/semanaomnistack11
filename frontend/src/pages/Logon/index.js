e2a22c518d111e1c6b5350fb8e51a041922dcfd0
Gustavo Nascimento <o_fantasma@hotmail.com.br>
2020-03-29 16:07:15 -0300

    Config the ESlint and Prettier

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 import { FiLogIn } from 'react-icons/fi';
 
-import "./styles.css";
+import './styles.css';
 
-import logoImg from "../../assets/logo.svg";
-import heroesImg from "../../assets/heroes.png";
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
   return (
