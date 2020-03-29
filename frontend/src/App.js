ee2dd9e63459c17903f24dfc326ebfcfa7432fa7
Gustavo Nascimento <o_fantasma@hotmail.com.br>
2020-03-29 13:04:14 -0300

    Add login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
-function App() {
+import './global.css';
+
+import Logon from './pages/Logon';
+
+function App () {
   return (
-    <h1>Hello World!!!</h1>
+   <Logon />
   );
 }
 
